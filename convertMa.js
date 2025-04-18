4776f751e13e72b3749a835904af6d877610d90a
unxoun <60313778+unxoun@users.noreply.github.com>
2025-04-18 20:53:44 +0330

    Update convertMa.js

--- convertMa.js
+++ convertMa.js
@@ -1,11 +1,13 @@
-function convertMA(L1, T1, T2) {
-    return L1 * (T2 / T1);
+// returns MA length in T2 time frame, equal to MA length in T1 time frame
+function convertMA(T1, L1, T2) {
+    return T1 > T2 ? L1 * (T1 / T2) : L1 * (T2 / T1);
 }
 
-// مثال استفاده:
-let L1 = 20; // مقدار MA در تایم فریم اولیه
-let T1 = 5;  // تایم فریم اولیه (دقیقه)
-let T2 = 1;  // تایم فریم هدف (دقیقه)
+// T1 = time frame 1 (minutes, eg 60)
+// L1 = MA in T1 (eg 21)
+// T2 = target time frame (eg 5)
 
-let L2 = convertMA(L1, T1, T2);
-console.log(`MA معادل در تایم فریم ${T2} دقیقه: ${L2}`);
+// get MA 21 in 1 houre in 5 min chart
+// what length should be MA in 5 min to represent the MA 21 in 1 houre chart
+// how do i see 1 houre MA-21 in 5 min chart:
+console.log(convertMA(60, 21, 5))
